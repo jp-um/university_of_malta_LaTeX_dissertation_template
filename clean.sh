7e2417d03bc8680d5e46bf95354b3f1f026a5438
JP <jean.p.ebejer@um.edu.mt>
2022-04-06 14:46:36 +0200

    added .git exclusion

--- clean.sh
+++ clean.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-find . -regex ".*\(aux\|bbl\|blg\|idx\|log\|lof\|lot\|nav\|out\|snm\|toc\|synctex.gz\)$" -exec rm -f {} \;
+# that .git exclusion is super important ...
+find . -regex ".*[^\.git].*\(aux\|bbl\|blg\|idx\|log\|lof\|lot\|nav\|out\|snm\|toc\|synctex.gz\)$" -not -path '*/.git/*' -exec rm -vf {} \;
