15a4707e8c4afd41e0b8572807085bdd185eee09
JP <jean.p.ebejer@um.edu.mt>
2024-11-18 14:46:28 +0100

    Fixed references to APA removed FICT template.

--- clean.sh
+++ clean.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # that .git exclusion is super important ...
-find . -regex ".*[^\.git].*\(aux\|bbl\|blg\|idx\|log\|lof\|lot\|nav\|out\|snm\|toc\|synctex.gz\)$" -not -path '*/.git/*' -exec rm -vf {} \;
+find . -regex ".*[^\.git].*\(aux\|bbl\|bcf\|blg\|idx\|ilg\|ind\|log\|lof\|lot\|nav\|out\|run.xml\|snm\|toc\|synctex.gz\)$" -not -path '*/.git/*' -exec rm -vf {} \;
